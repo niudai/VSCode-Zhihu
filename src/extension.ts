91a5bc5a629f54e74f94685cdd93cf8e7eb16f65
DaiNiu <1244453736@qq.com>
2019-10-15 15:15:14 +0800

    installed node-html-parser

--- src/extension.ts
+++ src/extension.ts
@@ -7,19 +7,47 @@ import { JsonOutlineProvider } from "./jsonOutline";
 import { FtpExplorer } from "./ftpExplorer";
 import { FileExplorer } from "./fileExplorer";
 import { TestView } from "./testView";
+import * as httpClient from 'request';
+import * as HtmlParser from 'node-html-parser';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
-		vscode.commands.registerCommand("nodeDependencies.openWebView", (innerHtml) => {
+		vscode.commands.registerCommand("nodeDependencies.openWebView", (questionId: number) => {
 			const panel = vscode.window.createWebviewPanel(
 				"nodeDependencies",
 				"node denpendencies",
 				vscode.ViewColumn.One,
 				{}
 			);
-			panel.webview.html = innerHtml;
-		})
-	);
+			let answerAPI = `https://www.zhihu.com/api/v4/questions/${questionId}/answers?include=${includeContent}?offset=${offset}`
+			httpClient(answerAPI, { json: true }, (err, _res, body) => {
+				console.log(answerAPI);
+				let answerHtmlRoots = body.data.map(answer => HtmlParser.parse(answer.content));
+				let imgAttrs = answerHtmlRoots.filter(root => root.querySelector('img')).map(root => root.querySelectorAll('img'));
+
+				// convert img src in answer inner html
+				imgAttrs.forEach(attr => {
+					attr.forEach(img => {
+						let src = imageUrlConverter(img.rawAttributes['data-actualsrc']);
+						img.rawAttributes.src = src;
+						img.rawAttributes.style = 'style = \"width: 90%\"';
+						img.rawAttrs = `src = \"${src}\" style = \"width: 90%\"`;
+						console.log(img.rawAttributes);
+						console.log(img.rawAttr);
+					}
+					);
+				});
+				// convert img src for avatar
+				body.data.forEach(answer => {
+					answer.author.avatar_url = imageUrlConverter(answer.author.avatar_url);
+				});
+				// convert 
+				body.data.forEach((answer, index) => answer.content = answerHtmlRoots[index].toString());
+				res.send(body);
+				panel.webview.html = innerHtml;
+			});
+		}
+		));
 	// Samples of `window.registerTreeDataProvider`
 	const nodeDependenciesProvider = new DepNodeProvider(
 		vscode.workspace.rootPath
